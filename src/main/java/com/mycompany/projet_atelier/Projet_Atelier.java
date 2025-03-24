9a45120d2f81df5c94cc3a3bc96565811a13f5e1
alice-dbv <deboever.alice@gmail.com>
2025-03-24 17:23:57 +0100

    first commit

---
+++ src/main/java/com/mycompany/projet_atelier/Projet_Atelier.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projet_atelier;
+
+/**
+ *
+ * @author alicedeboever
+ */
+public class Projet_Atelier {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
