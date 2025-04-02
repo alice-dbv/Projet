baaec9dd566f2df066caac39c8ee97658ae3b8b4
lisaa <lisaa@172.29.69.63>
2025-04-02 10:36:58 +0200

    créeation classe machine

--- src/main/java/com/mycompany/projet_atelier/Machine.java
+++ src/main/java/com/mycompany/projet_atelier/Machine.java
@@ -2,13 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.mycompany.projet_atelier;
 
 /**
  *
  * @author lisaa
  */
 public class Machine {
-   private  String refMachine;
+    private  String refMachine;
    private String dMachine;
    private float x;
    private float y;
